069ff4e1950a83b1c9b9e6294e31fc0b32c538b8
Michelle Strout <mstrout@github.com>
2017-12-11 14:03:45 -0700

    Driver for Java Hello World program.

---
+++ JavaHelloWorldMain.java
@@ -0,0 +1,24 @@
+/**
+ * JavaHelloWordMain.java
+ * 
+ * Main driver for hello world program.
+ * 
+ * usage:
+ * 	running in Eclipse
+ */
+
+/**
+ * @author mstrout
+ *
+ */
+public class JavaHelloWorldMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}

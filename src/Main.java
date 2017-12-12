aead84fbac968a5aae7359505e97a84c2a596f74
Michelle Strout <mstrout@cs.arizona.edu>
2017-12-12 11:20:51 -0700

    Created an eclipse project.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mstrout
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("testing");
+
+	}
+
+}
